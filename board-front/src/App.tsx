c5bf18d7ff4d70e7f8f1a1185f41992af84d8638
진서형 <jinsheo107@gmail.com>
2025-02-04 19:24:10 +0900

    path & 404

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -9,6 +9,8 @@ import BoardDetail from 'views/Board/Detail';
 import BoardWrite from 'views/Board/Write';
 import BoardUpdate from 'views/Board/Update';
 import Container from 'layouts/Container';
+import { MAIN_PATH, AUTH_PATH, SEARCH_PATH, USER_PATH, BOARD_PATH, BOARD_WRITE_PATH, BOARD_DETAIL_PATH, BOARD_UPDATE_PATH } from 'constant';
+
 
 // component: Application Component
 function App() {
@@ -27,15 +29,18 @@ function App() {
   return (
     <Routes>
       <Route element={<Container />}>
-        <Route path='/' element={<Main />} />
-        <Route path='/auth' element={<Authentication />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/user' element={<User />} />
-        <Route path='/board'>
-          <Route path='write' element={<BoardWrite />} />
-          <Route path='update/:boardNo' element={<BoardUpdate />} />
-          <Route path='detail/:boardNo' element={<BoardDetail />} />
+        <Route path={MAIN_PATH()} element={<Main />} />
+        <Route path={AUTH_PATH()} element={<Authentication />} />
+        <Route path={SEARCH_PATH(':searchWord')} element={<Search />} />
+        <Route path={USER_PATH(':userEmail')} element={<User />} />
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+          <Route path={BOARD_DETAIL_PATH(':boardNo')} element={<BoardDetail />} />
+          <Route path={BOARD_UPDATE_PATH(':boardNo')} element={<BoardUpdate />} />
         </Route>
+
+        // 404 NOT FOUND
+        <Route path='*' element={<h1>404 NOT FOUND</h1>} />
       </Route>
     </Routes>
   );
--- board-front/src/layouts/Container/index.tsx
+++ board-front/src/layouts/Container/index.tsx
@@ -2,6 +2,7 @@ import Footer from 'layouts/Footer'
 import Header from 'layouts/Header'
 import { Outlet, useLocation } from 'react-router-dom'
 import React from 'react'
+import { AUTH_PATH } from 'constant';
 
 export default function Container() {
 
@@ -12,7 +13,7 @@ export default function Container() {
     <>
       <Header />
       <Outlet/>
-      {pathname !== '/auth' && <Footer/>}
+      {pathname !== AUTH_PATH() && <Footer/>}
     </>
   )
 }
